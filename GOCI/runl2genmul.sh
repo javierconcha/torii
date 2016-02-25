9d89a37c4e4d1a3512fe54a05ed23a64f9f4704e
Javier Conchas <jconchas@anly104.domain.sdps>
2016-02-24 20:03:11 -0500

    Created script to run multiple l2gen, but it generates a file_list based on an input range...

--- GOCI/runl2genmul.sh
+++ GOCI/runl2genmul.sh
@@ -3,11 +3,13 @@
 # Javier A. Concha
 # 2016-01-13
 
-echo "Type the range that you want to run (4 digits), followed by [ENTER]:"
+echo "Type the r that you want to run (4 digits), followed by [ENTER]:"
 
 read range
 
-for FILE in `cat file_list.txt`
+sed -n "${range:0:2}","${range:2:2}"p file_list.txt > file_list"$range".txt
+
+for FILE in `cat file_list"$range".txt`
 do 	
 	echo running l2gen on $FILE 	
 	l2gen ifile=./"$FILE"  ofile1=./"$FILE"_L2.nc l2prod="default,ag_412_mlrc"
