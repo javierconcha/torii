92d185a1ccd8950c1455a7b974bf96a7bb91333a
Javier Conchas <jconchas@anly104.domain.sdps>
2016-06-07 13:11:56 -0400

    runl2genmul_crop.sh working now...

--- LANDSAT8/L8_Rrs_Matchups_AERONET/runl2genmul_crop.sh
+++ LANDSAT8/L8_Rrs_Matchups_AERONET/runl2genmul_crop.sh
@@ -10,15 +10,35 @@ read range
 
 if [ ${range:3:3} -ne "000" ]
 then
-	sed -n "${range:0:3}","${range:3:3}"p file_list.txt > file_list"$range".txt
+	sed -n "${range:0:3}","${range:3:3}"p idlatlon_list.txt > idlatlon_list"$range".txt
 else
-	sed -n "${range:0:3}","${range:0:3}"p file_list.txt > file_list"$range".txt
+	sed -n "${range:0:3}","${range:0:3}"p idlatlon_list.txt > idlatlon_list"$range".txt
 fi
 
-for FILE in `cat file_list"$range".txt`
+
+IFS=$'\n'       # make newlines the only separator
+for LINE in `cat idlatlon_list"$range".txt`
 do 	
+	FILE=`echo $LINE | awk -F ' ' '{print $1}'`
+	LAT=`echo $LINE | awk -F ' ' '{print $2}'`
+	LON=`echo $LINE | awk -F ' ' '{print $3}'`
+
+	echo $FILE $LAT $LON
+
+	lonlat2pixline -x 50 -y 50 ./"$FILE"/"$FILE"_MTL.txt "$LON" "$LAT" > ./"$FILE"/"$FILE"_lonlat2pixline.o
+	eval "$(grep "sline" ./"$FILE"/"$FILE"_lonlat2pixline.o)"
+	eval "$(grep "eline" ./"$FILE"/"$FILE"_lonlat2pixline.o)"
+	eval "$(grep "spixl" ./"$FILE"/"$FILE"_lonlat2pixline.o)"
+	eval "$(grep "epixl" ./"$FILE"/"$FILE"_lonlat2pixline.o)"
+	#rm ./"$FILE"/"$FILE"_lonlat2pixline.o
+
+	echo $sline $eline $spixl $epixl
+
 	echo running l2gen on $FILE 	
-	l2gen ifile=./"$FILE"/"$FILE"_MTL.txt ofile1=./"$FILE"/"$FILE"_L2n1SWIR5x5.nc aer_opt=-1 ctl_pt_incr=1 maskglint=1 l2prod="default,ag_412_mlrc,rhos_vvv" aer_wave_long=2201 aer_wave_short=1609 filter_file=/home/jconchas/ocssw/run/data/oli/msl12_filterUSER.dat filter_opt=1
+	
+	l2gen ifile=./"$FILE"/"$FILE"_MTL.txt ofile1=./"$FILE"/"$FILE"_L2n1.nc aer_opt=-1 ctl_pt_incr=1 maskglint=1 l2prod="default,ag_412_mlrc,rhos_vvv"  sline="$sline" eline="$eline" spixl="$spixl" epixl="$epixl"
+
+#l2gen ifile=./"$FILE"/"$FILE"_MTL.txt ofile1=./"$FILE"/"$FILE"_L2n1SWIR5x5.nc aer_opt=-1 ctl_pt_incr=1 maskglint=1 l2prod="default,ag_412_mlrc,rhos_vvv" aer_wave_long=2201 aer_wave_short=1609 filter_file=/home/jconchas/ocssw/run/data/oli/msl12_filterUSER.dat filter_opt=1
 
 	let i++
 	echo $i
