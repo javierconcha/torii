22ce8f73e87fa6a7a1eace4cd8be9548d4b33cad
Javier Conchas <jconchas@anly104.domain.sdps>
2016-06-24 14:24:06 -0400

    scheduler.sh working

--- GOCI/runl2genmul_KORUS_OC.sh
+++ GOCI/runl2genmul_KORUS_OC.sh
@@ -3,16 +3,11 @@
 # Javier A. Concha
 # Created 2016-06-21 (Finally Spring!)
 
-echo "Type the r that you want to run (4 digits), followed by [ENTER]:"
+#echo "Type the r that you want to run (4 digits), followed by [ENTER]:"
 
-read range
+#read range
 
-if [ ${range:3:3} -ne "000" ]
-then
-        sed -n "${range:0:3}","${range:3:3}"p file_list.txt > file_list"$range".txt
-else
-        sed -n "${range:0:3}","${range:0:3}"p file_list.txt > file_list"$range".txt
-fi
+sed -n lim1,lim2p file_list.txt > file_listlim1lim2.txt
 
 # coordinates for South Korea Peninsula
 SWlon=122
@@ -20,9 +15,9 @@ SWlat=32
 NElon=132
 NElat=41
 
-
-for FILE in `cat file_list"$range".txt`
-do 	
+#for FILE in `cat file_list"$range".txt`
+for FILE in `cat file_listlim1lim2.txt`
+do
 	echo running l2gen on $FILE 	
 	
 	lonlat2pixline ./"$FILE" "$SWlon" "$SWlat" "$NElon" "$NElat" > ./"$FILE"_lonlat2pixline.o
@@ -34,9 +29,9 @@ do
 	
 	echo $sline $eline $spixl $epixl
 
-	l2gen ifile=./"$FILE"  ofile1=./"$FILE"_L2n1.nc aer_opt=-1 ctl_pt_incr=1 maskglint=1  l2prod="default,ag_412_mlrc,rhos_vvv" sline="$sline" eline="$eline" spixl="$spixl" epixl="$epixl" 
+	l2gen ifile=./"$FILE"  ofile1=./"$FILE"_L2n2.nc aer_opt=-2 ctl_pt_incr=1 maskglint=1  l2prod="default,ag_412_mlrc,rhos_vvv" sline="$sline" eline="$eline" spixl="$spixl" epixl="$epixl" 
 
 	let i++
 	echo $i
 done
-echo l2gen run in multiple files finished...
+echo l2gen run in multiple finished...
--- GOCI/scheduler.sh
+++ GOCI/scheduler.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Script to schedule multiple jubs using different tmux windows within a tmux session
+# Created by Javier Concha, 2016-06-24
+
+# number of jobs per window
+n_jobs=10
+
+# tmux configuration
 # var for session name (to avoid repeated occurences)
 sn=0
 
@@ -10,9 +17,21 @@ tmux new-session -s "$sn" -n etc -d
 
 # Create a bunch of windows in /var/log
 cd ~/data2/GOCI/
-for i in {1..2}; do
+
+line_no=$(wc -l < file_list.txt)
+max=$((line_no/n_jobs+1))
+
+for (( i = 1; i <= $max; i++ )) 
+do
+	echo $i
+	LIM1=$(((i-1)*n_jobs+1))
+	LIM2=$((i*n_jobs))
+	cat runl2genmul_KORUS_OC.sh | sed 's"lim1"'$LIM1'"g' | sed 's"lim2"'$LIM2'"g' > runl2genmul_temp$LIM1$LIM2.sh
+	
+	chmod 755 ./runl2genmul_temp$LIM1$LIM2.sh
+	
 	tmux new-window -t "$sn:$i" -n "var$i"
-	tmux send-keys -t "$sn:$i" C-z './temp_echo.sh' Enter
+	tmux send-keys -t "$sn:$i" C-z './runl2genmul_temp'$LIM1''$LIM2'.sh' Enter
 done
 
 # Set the default cwd for new windows (optional, otherwise defaults to session cwd)
@@ -21,3 +40,4 @@ done
 # Select window #1 and attach to the session
 tmux select-window -t "$sn:0"
 tmux -2 attach-session -t "$sn"
+
