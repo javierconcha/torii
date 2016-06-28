39ef048a6c50ce8b758cd27e5fbde07c3615d23a
Javier Conchas <jconchas@anly104.domain.sdps>
2016-06-28 17:46:08 -0400

    download all scene that are not already downloaded

--- LANDSAT8/L8_Rrs_Matchups_AERONET/L8_download_all_scenes_in_list.py
+++ LANDSAT8/L8_Rrs_Matchups_AERONET/L8_download_all_scenes_in_list.py
@@ -15,58 +15,53 @@ import sys
 
 #  NOTE to download L7 scenes you need to modify this code and use different ports
 
-infile = "scene_list.txt"
+infile = "scene_listAERONET_all.txt"
 f1 =  open(infile, 'r+')
 lines = f1.readlines()
 
 # Loop through all Landsat filenames in input list
 for n in lines:
-    print n[0:21]
-    L8filename =  n[0:21]
+    
+	print n[0:21]
+    	L8filename =  n[0:21]
+	if not os.path.exists("./"+L8filename):
+		##########################################################################################
+		# ATTN YOU MUST ENTER YOUR OWN Earth Explorer Username and Password below XXXXX  YYYYYY	 #
+		##########################################################################################    
+    		image_call_step1 = "wget https://ers.cr.usgs.gov/login/ --save-cookies cookies.txt --keep-session-cookies --post-data 'username=jaconcha&password=Odi33i69' --no-check-certificate"
 
-##########################################################################################
-# ATTN YOU MUST ENTER YOUR OWN Earth Explorer Username and Password below XXXXX  YYYYYY	 #
-##########################################################################################    
-    image_call_step1 = "wget https://ers.cr.usgs.gov/login/ --save-cookies cookies.txt --keep-session-cookies --post-data 'username=jaconcha&password=Odi33i69' --no-check-certificate"
-
-    os.system(image_call_step1)
+    		os.system(image_call_step1)
 	
-    image_call_step2 ="wget --load-cookies cookies.txt earthexplorer.usgs.gov/download/4923/"+L8filename+"/STANDARD/EE/ --no-check-certificate --output-document="+L8filename+".tar.gz"
-    print image_call_step2
-    status = os.system(image_call_step2)
-    print " img download status = ", status, " a zero means file exists"
-    # if download fails	
-    if status != 0:
- 	    print "**************************************"
- 	    print "* WARNING image was not on site      *"
- 	    print "**************************************"
-    # if download successful
-    if status == 0:
-	    print "Image EXISTS on ftp site. Download successful"
-	    print " Uncompressing tar.gz files ...."
-	    os.system("mkdir "+L8filename)   	
-            os.system("tar -zxvf "+L8filename+".tar.gz")
-	    os.system("mv "+L8filename+"* "+L8filename+"/" )   
-	    # Clean up unused files
-            # comment out which files you want to keep
-	    # os.system("rm *tar.gz")
-  	    # os.system("rm *_B1.TIF")
-	    # os.system("rm *_B2.TIF")
-	    # os.system("rm *_B3.TIF")
-	    # os.system("rm *_B4.TIF")
-	    # os.system("rm *_B5.TIF")
-	    # os.system("rm *_B6.TIF")
-	    # os.system("rm *_B7.TIF")
-	    # os.system("rm *_B8.TIF")
-            # os.system("rm *_B9.TIF")
-	    # Keep just the Thermal Bands
-            # os.system("rm *_B10*")
-	    # os.system("rm *_B11*")
-	    # Keep BQA for L8 cloud detection
-            # os.system("rm *_BQA*")
-            # gap for L7
-            # os.system("rm -R *gap*")
+    		image_call_step2 ="wget --load-cookies cookies.txt earthexplorer.usgs.gov/download/4923/"+L8filename+"/STANDARD/EE/ --no-check-certificate --output-document="+L8filename+".tar.gz"
+    		print image_call_step2
+    		status = os.system(image_call_step2)
+    		print " img download status = ", status, " a zero means file exists"
+    		# if download fails	
+    		if status != 0:
+ 	    		print "**************************************"
+ 	    		print "* WARNING image was not on site      *"
+ 	    		print "**************************************"
+			os.system("echo "+L8filename+" >> file_not_on_site.txt")
+    		# if download successful
+    		if status == 0:
+	    		print "Image EXISTS on ftp site. Download successful"
+	    		print " Uncompressing tar.gz files ...."
+	    		os.system("mkdir "+L8filename)   	
+            		os.system("tar -zxvf "+L8filename+".tar.gz")
+	    		
+			# Clean up unused files
+            		# comment out which files you want to keep
+	    		os.system("rm *tar.gz")
+	    		# Keep just the Thermal Bands
+            		os.system("rm *_B10*")
+	    		os.system("rm *_B11*")
+	    		# Keep BQA for L8 cloud detection
+            		os.system("rm *_BQA*")
+			os.system("echo "+L8filename+" >> file_on_site.txt")
 
+			os.system("mv "+L8filename+"* "+L8filename+"/" )
+	else:
+		os.system("echo "+L8filename+" >> file_already_downloaded.txt")
 f1.close()
 
 
